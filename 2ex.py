2ef29ad744e63eb31234b05a4c28898c27b034b7
Maalina3 <lina_grishaeva773@mail.ru>
2026-07-06 13:20:55 +0300

    начало второго файла

--- 2ex.py
+++ 2ex.py
@@ -6,10 +6,20 @@ for i in range(1, n + 1):
     numbers.append(i)
 numbers *= 2
 
-copy_numbers = numbers
+#copy_numbers = numbers
+delete_index = 1 #индекс элемента, который будем удалять
 
-delete_index = 1
-for i in range(3):
-    pass
-    
+while len(numbers) > 2: #[1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4]
+    next_died = numbers[delete_index]
+    next_killer = numbers[delete_index + 1] #само число, после которого далее следует удаление #9
+    #print(next_died)
+
+    copy_numbers = [i for i in numbers if i != next_died]
+    numbers = copy_numbers
+    #print(numbers)
+    #print()
+
+    delete_index = numbers.index(next_killer) + 1
+
+print(numbers[0])    
     
