7e042764ed99c3b232fd4466826d07544b077307
Maalina3 <lina_grishaeva773@mail.ru>
2026-07-10 13:30:57 +0300

    решила вторую задачу со случайным K

--- 2ex.py
+++ 2ex.py
@@ -1,25 +1,20 @@
-n = 12
-k = 2
+n = int(input())
+k = int(input())
 numbers = []
 
 for i in range(1, n + 1):
     numbers.append(i)
-numbers *= 2
+numbers *= k
 
-#copy_numbers = numbers
-delete_index = 1 #индекс элемента, который будем удалять
+delete_index = k - 1 #индекс элемента, который будем удалять
 
-while len(numbers) > 2: #[1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4]
-    next_died = numbers[delete_index]
-    next_killer = numbers[delete_index + 1] #само число, после которого далее следует удаление #9
-    #print(next_died)
+while len(numbers) > k: #[1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4]
+    next_died = numbers[delete_index] #Находим число, которое будем удалять
+    next_killer = numbers[delete_index + 1] #Число, которое стоит после удаленного и с которого будем начинать последующий отсчет
 
-    copy_numbers = [i for i in numbers if i != next_died]
-    numbers = copy_numbers
-    #print(numbers)
-    #print()
+    numbers = [i for i in numbers if i != next_died] #Создаем новый список без удаленного числа
+    delete_index = numbers.index(next_killer) + (k-1) #Отсчитываем в новом списке индекс, по которомо будет стоять следующее удаленное число
 
-    delete_index = numbers.index(next_killer) + 1
-
-print(numbers[0])    
+print(numbers[0])
+    
     
